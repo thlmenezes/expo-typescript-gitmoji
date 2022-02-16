9452bd69ae1919d536ded4a72fd6847bbc4a7db7
Thales Menezes <thlmenezes@pm.me>
2022-02-16 16:02:36 -0300

    ✨ update Card

--- src/atomic/molecules/Card/index.tsx
+++ src/atomic/molecules/Card/index.tsx
@@ -5,14 +5,14 @@ import { RoundImage } from "@root/atomic/atoms";
 import { CardTitle, Container } from "./styles";
 
 export interface CardProps {
-  image: string;
+  image: number | { uri: string };
   title: string;
 }
 
 export const Card = ({ image, title }: CardProps) => {
   return (
     <Container>
-      <RoundImage source={{ uri: image }} />
+      <RoundImage source={image} />
       <CardTitle>{title}</CardTitle>
     </Container>
   );
