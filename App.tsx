74353c2c6bec5817d3c982595e6b8cadcf05d7a5
Thales Menezes <thlmenezes@pm.me>
2022-02-16 15:08:24 -0300

    💄 enable ThemeProvider
    needs to update app.json to access user device theme choice

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
 import * as React from "react";
+import { useColorScheme } from "react-native";
+import { ThemeProvider } from "styled-components/native";
 
+import { themeSelector } from "@root/global";
 import { Routes } from "@root/routes";
 
 export default function App() {
+  const colorScheme = useColorScheme();
+
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <ThemeProvider theme={themeSelector[colorScheme ?? "dark"]}>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
