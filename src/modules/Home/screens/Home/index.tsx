d02b86826cc9cbedbfa209f6be23f4cb5b7e4eca
Thales Menezes <thlmenezes@pm.me>
2022-02-17 16:42:21 -0300

    ✨ add buttons to navigate between screens

--- src/modules/Home/screens/Home/index.tsx
+++ src/modules/Home/screens/Home/index.tsx
@@ -1,16 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { TouchableOpacity } from "react-native";
 
 import { icons } from "@root/assets";
 import { Title, RoundImage } from "@root/atomic/atoms";
 import { Card } from "@root/atomic/molecules";
+import { HomeProps } from "@root/routes";
 
 import { Container, Text } from "./styles";
 
-export const Home = () => {
+export const Home = ({ navigation }: HomeProps) => {
+  function handlePress() {
+    navigation.navigate("User");
+  }
+
   return (
     <Container>
-      <Card image={icons.icon} title="Home" />
+      <TouchableOpacity onPress={handlePress}>
+        <Card image={icons.icon} title="Home" />
+      </TouchableOpacity>
       <Text>Change Default Device Color to See Magic</Text>
       <StatusBar style="auto" />
     </Container>
--- src/modules/User/screens/UserList/index.tsx
+++ src/modules/User/screens/UserList/index.tsx
@@ -1,16 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { TouchableOpacity } from "react-native";
 
 import { icons } from "@root/assets";
 import { Title, RoundImage } from "@root/atomic/atoms";
 import { Card } from "@root/atomic/molecules";
+import { UserProps } from "@root/routes";
 
 import { Container, Text } from "./styles";
 
-export const UserList = () => {
+export const UserList = ({ navigation }: UserProps) => {
+  function handlePress() {
+    navigation.navigate("Home");
+  }
+
   return (
     <Container>
-      <Card image={icons.icon} title="UserList" />
+      <TouchableOpacity onPress={handlePress}>
+        <Card image={icons.icon} title="UserList" />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </Container>
   );
