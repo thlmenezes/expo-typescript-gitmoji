7cbb626171a71dbdc284db528e3cedf8b1ec69d2
Thales Menezes <thlmenezes@pm.me>
2022-02-17 16:41:38 -0300

    ✨ connect User routing to app routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,29 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 
 import { HomeRoutes } from "@root/modules/Home";
+import { UserRoutes } from "@root/modules/User";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+type RootStackParamList = {
+  Home: undefined;
+  User: undefined;
+};
+
+export type UserProps = NativeStackScreenProps<RootStackParamList, "User">;
+export type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">;
 
 export const Routes = () => {
-  return <HomeRoutes />;
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Screen name="Home" component={HomeRoutes} />
+      <Screen name="User" component={UserRoutes} />
+    </Navigator>
+  );
 };
