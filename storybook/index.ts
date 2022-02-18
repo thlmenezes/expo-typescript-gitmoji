abd21444b520a354e48076ef34d4b71bc0d627e5
Thales Menezes <thlmenezes@pm.me>
2022-02-18 13:06:26 -0300

    🐛 minor errors and warnings

--- storybook/index.ts
+++ storybook/index.ts
@@ -1,7 +1,5 @@
-// if you use expo remove this line
 import { withKnobs } from "@storybook/addon-knobs";
 import { getStorybookUI, configure, addDecorator } from "@storybook/react-native";
-import { AppRegistry } from "react-native";
 
 import "./rn-addons";
 
@@ -15,10 +13,8 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
-
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
-// If you use Expo you should remove this line.
-AppRegistry.registerComponent("%APP_NAME%", () => StorybookUIRoot);
+const StorybookUIRoot = getStorybookUI({
+  asyncStorage: null,
+});
 
 export default StorybookUIRoot;
