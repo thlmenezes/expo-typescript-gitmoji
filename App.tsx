59d6e3ab6837d1ceafafc3b6a3148b83ada225b8
Thales Menezes <thlmenezes@pm.me>
2022-02-17 22:41:34 -0300

    ✨ QueryClientProvider

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import * as React from "react";
 import { useColorScheme } from "react-native";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components/native";
 
 import { themeSelector } from "@root/global";
 import { Routes } from "@root/routes";
 
+const client = new QueryClient();
+
 export default function App() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider theme={themeSelector[colorScheme ?? "dark"]}>
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider theme={themeSelector[colorScheme ?? "dark"]}>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
