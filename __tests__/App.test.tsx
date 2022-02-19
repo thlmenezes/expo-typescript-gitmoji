7121800bddc354da68091e6c40993fb06b361ec0
Thales Menezes <thlmenezes@pm.me>
2022-02-19 03:27:32 -0300

    ✅ use @testing-library/react-native

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,11 +1,11 @@
+import { render } from "@testing-library/react-native";
 import React from "react";
-import renderer from "react-test-renderer";
 
 import App from "../App";
 
 describe("<App />", () => {
   it("has 1 child", () => {
-    const tree: any = renderer.create(<App />).toJSON();
+    const tree: any = render(<App />).toJSON();
     expect(tree?.children?.length).toBe(1);
   });
 });
